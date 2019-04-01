c63e47275ae498a3d6ce0a4de4a794f6c038664d
aklcqq <48574784+aklcqq@users.noreply.github.com>
2019-04-01 20:09:50 +0800

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,13 +1,16 @@
 num = 0
 count = 0
+total = 0
 while (True):
     num = input('Enter a number:')
     if num == 'done': break
     try:
         val = float(num)
+        count = count + 1
+        total = val + total
+        average = total / count
+
     except:
         print('Invalid input')
-    total = val + total
-    count = count + 1
-average = total / count
+
 print('average',average,'count',count,'sum',total)
