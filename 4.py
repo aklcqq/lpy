72530b6274742bba09b63b4bf44ec89c8c7b3308
aklcqq <48574784+aklcqq@users.noreply.github.com>
2019-04-01 20:50:31 +0800

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,11 +1,15 @@
-num = 0
-max = num
-min = num
+try:
+    num = input('Enter a number:')
+    num = float(num)
+    max = num
+    min = num
+except:
+    print('Invalid input')
 while (True):
     num = input('Enter a number:')
     if num == 'done': break
     try:
-        val = float(num) 
+        val = float(num)
         if val >= max:
             max = val
         if val <= min:
