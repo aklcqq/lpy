f860e3eb40346d3b137f341c2d8c7b7d7f325d7e
aklcqq <48574784+aklcqq@users.noreply.github.com>
2019-04-02 13:58:17 +0800

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,1 +1,11 @@
-print('h')
+wordlist = []
+fname = input('Enter file:')
+fhand = open(fname)
+for line in fhand:
+    h = line.rstrip()
+    words = h.split()
+    for word in words:
+        if word in wordlist: continue
+        wordlist.append(word)
+wordlist.sort()
+print(wordlist)
