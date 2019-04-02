3c7dfefcf728118acd0c89f471d5933380da5a9d
aklcqq <48574784+aklcqq@users.noreply.github.com>
2019-04-02 14:20:06 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,1 +1,12 @@
-print('hello world')
+num = []
+while (True):
+    val = input('Enter a number:')
+    if val == 'done':
+        break
+    else:
+        try:
+            val = float(val)
+            num.append(val)
+        except:
+            print('invalid input')
+print('max:',max(num),'\nmin',min(num))
